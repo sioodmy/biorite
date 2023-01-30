db1e3bbf329e96b9446b4feba6d30d7a0f8d70c7
sioodmy <hello@sioodmy.dev>
2023-01-30 22:27:01 +0100

    feat: mold linker

--- flake.nix
+++ flake.nix
@@ -40,6 +40,7 @@
 
         build-deps = with pkgs; [
           llvmPackages.bintools
+          mold
           lld
           clang
           pkg-config
@@ -49,6 +50,7 @@
           jemalloc
         ];
         runtime-deps = with pkgs; [
+          # TODO: remove unused libs
           alsa-lib
           udev
           xorg.libX11
@@ -56,10 +58,19 @@
           xorg.libXrandr
           xorg.libXi
           xorg.libxcb
-          xlibsWrapper
           libGL
+          freetype
+          fontconfig
+          xorg.xorgproto
+          xorg.libXt
+          xorg.libXft
+          xorg.libXext
+          xorg.libSM
+          xorg.libICE
           vulkan-loader
           vulkan-headers
+          vulkan-validation-layers
+          vulkan-tools
           libxkbcommon
           wayland
           glfw-wayland
@@ -87,13 +98,37 @@
         apps.default = apps.biorite;
 
         # For `nix develop`:
-        devShell = with pkgs;
-          mkShell {
+        devShell = with pkgs; let
+          # https://discourse.nixos.org/t/using-mold-as-linker-prevents-libraries-from-being-found/18530/4
+          bintools-wrapper = "${nixpkgs}/pkgs/build-support/bintools-wrapper";
+          mold' = symlinkJoin {
+            name = "mold";
+            paths = [mold];
+            nativeBuildInputs = [pkgs.makeWrapper];
+            suffixSalt = lib.replaceStrings ["-" "."] ["_" "_"] targetPlatform.config;
+            postBuild = ''
+              for bin in ${mold}/bin/*; do
+                rm $out/bin/"$(basename "$bin")"
+
+                export prog="$bin"
+                substituteAll "${bintools-wrapper}/ld-wrapper.sh" $out/bin/"$(basename "$bin")"
+                chmod +x $out/bin/"$(basename "$bin")"
+
+                mkdir -p $out/nix-support
+                substituteAll "${bintools-wrapper}/add-flags.sh" $out/nix-support/add-flags.sh
+                substituteAll "${bintools-wrapper}/add-hardening.sh" $out/nix-support/add-hardening.sh
+                substituteAll "${bintools-wrapper}/../wrapper-common/utils.bash" $out/nix-support/utils.bash
+              done
+            '';
+          };
+        in
+          mkShell.override {stdenv = gcc12Stdenv;} {
             RUST_SRC_PATH = rustPlatform.rustLibSrc;
+            NIX_CFLAGS_LINK = "-fuse-ld=mold";
             RUST_LOG = "info,wgpu_core=warn,wgpu_hal=off,rechannel=warn,biorite=debug";
-            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath runtime-deps;
             shellHook = ''
               mkdir -p world/regions
+              export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath runtime-deps}"
             '';
             buildInputs =
               [toolchain pciutils]
